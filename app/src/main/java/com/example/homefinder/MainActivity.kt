bce89c8e0d9dcbe724be37a4ed3dea54a65b903d
altanm <mehmetturkmen200@hotmail.com>
2021-09-26 21:38:20 +0300

    viewpager added

--- app/src/main/java/com/example/homefinder/MainActivity.kt
+++ app/src/main/java/com/example/homefinder/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.homefinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homefinder.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val adapter = ViewPagerAdapter(supportFragmentManager,lifecycle)
+        binding.viewPager2.adapter = adapter
+
+        TabLayoutMediator(binding.tabView,binding.viewPager2){tab,position ->
+           when(position){
+               0->{
+                   tab.text = "Login"
+               }
+               1 -> {
+                   tab.text = "SignUp"
+               }
+           }
+        }.attach()
+
     }
 }
