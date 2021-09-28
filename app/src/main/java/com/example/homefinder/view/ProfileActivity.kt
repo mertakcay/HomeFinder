054f0fc49c07d089cd778e4e1a41ef3d58ee2ff2
altanm <mehmetturkmen200@hotmail.com>
2021-09-28 15:23:01 +0300

    ProfileActivity.kt added

---
+++ app/src/main/java/com/example/homefinder/view/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.homefinder.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homefinder.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
